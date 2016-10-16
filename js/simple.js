99f06996389a02be4750e7ced47c1d34d69497f0
Erick Friis <efriis@mit.edu>
2016-10-16 19:46:14 -0400

    simple mode

--- js/simple.js
+++ js/simple.js
@@ -1,3 +1,11 @@
 /**
  * Created by efriis on 10/16/16.
  */
+"use strict";
+var lib_1 = require("./lib");
+var wf = new lib_1.WordFinder();
+process.stdin.on("data", function (data) {
+    var d = data.toString();
+    var rtn = wf.search(d.substring(0, d.length - 1));
+    console.log(rtn.join(" "));
+});
